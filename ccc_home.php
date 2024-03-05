5a4e13b9b58db6f4990c5557bc43dfefb3bde032
Meghan-Kelly <152535260+Meghan-Kelly@users.noreply.github.com>
2024-03-05 13:32:48 -0500

    Initial Commit

---
+++ ccc_home.php
@@ -0,0 +1,29 @@
+<!-- Chapter 2, Exercise 2-5
+	Coast City Computers Home Page -->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Coast City Computers</title>
+</head>
+<body style="background-color: lightgray;">
+	
+	<?php include "ccc_header.php"?>
+
+	<h2>Presidents'Day Sale 2024</h2>
+	<ul>
+		<li>Compaq Presario Laptop: <Strong>$$799.99</Strong></li>
+		<li>Epson Stylus CX6600 Color Printer: <Strong>$440.99</Strong></li>
+		<li>Proview Technology 24-inch Monitor: <strong>$199.75</strong></li>
+		<li>Hawking Technology Wireless Card: <Strong>
+		$14.50</Strong></li>
+	</ul>
+	<br/>
+	<br/>
+	<a href="ccc_contact.php">Click to Contact us!</a>
+	
+	<?php include "ccc_footer.php"?>
+</body>
+</html>
